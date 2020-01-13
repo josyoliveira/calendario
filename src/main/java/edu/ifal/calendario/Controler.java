015b3ef306affa55e4055544cdbec1a413bbdea6
josyoliveira <joseane.olliveira52@gmail.com>
2020-01-13 16:18:32 -0300

    Implementacao dos teste e modificando o metodo proximoDia para que todos os teste funcione

--- src/main/java/edu/ifal/calendario/Controler.java
+++ src/main/java/edu/ifal/calendario/Controler.java
@@ -5,16 +5,42 @@ public class Controler {
     public Data proximoDia(Data dataAtual) {
         Data diaSeguinte = new Data(0, 0, 0);
 
-        if (dataAtual.getMes() % 2 != 0 && dataAtual.getDia() == 31) {
-            diaSeguinte.setMes(1);
+        if (dataAtual.getDia() == getUltimoDiaDoMes(dataAtual.getMes(), dataAtual.getAno())) {
+            diaSeguinte.setDia(1);
             diaSeguinte.setMes(dataAtual.getMes() + 1);
+            diaSeguinte.setAno(dataAtual.getAno());
+            if (dataAtual.getMes() == 12) {
+                diaSeguinte.setMes(1);
+                diaSeguinte.setAno(dataAtual.getAno() + 1);
+            }
         } else {
             diaSeguinte.setDia(dataAtual.getDia() + 1);
             diaSeguinte.setMes(dataAtual.getMes());
+            diaSeguinte.setAno(dataAtual.getAno());
         }
-        diaSeguinte.setAno(dataAtual.getAno());
 
         return diaSeguinte;
+    }
+
+    private int getUltimoDiaDoMes(int mes, int ano) {
+        int ultimoDia;
+        int meses[] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+        if (mes == 2 && ehBissexto(ano)) {
+            ultimoDia = 29;
+        } else
+            ultimoDia = meses[mes - 1];
+        return ultimoDia;
+
+    }
+
+    private boolean ehBissexto(int ano) {
+        boolean bissexto = false;
+        if (ano % 4 == 0 && ano % 100 != 0) {
+            bissexto = true;
+        } else if (ano % 400 == 0) {
+            bissexto = true;
+        }
+        return bissexto;
     }
 
     // public Data proximoDiaEMes(Data dataAtual) {
--- src/test/java/edu/ifal/calendario/ControlerTest.java
+++ src/test/java/edu/ifal/calendario/ControlerTest.java
@@ -7,15 +7,15 @@ import org.junit.Test;
 public class ControlerTest {
 
     @Test
-    public void verificarProximoDiaDentroDoMesmoMes() {
+    public void verificarProximoDiaDentroDoMes31DiasDepoisDoDia30() {
         Controler controler = new Controler();
-        Data proximoDiaRetornado = controler.proximoDia(new Data(1, 1, 2020));
-        Data proximoDiaEsperado = new Data(2, 1, 2020);
+        Data proximoDiaRetornado = controler.proximoDia(new Data(30, 3, 2020));
+        Data proximoDiaEsperado = new Data(31, 3, 2020);
         assertEquals(proximoDiaEsperado, proximoDiaRetornado);
     }
 
     @Test
-    public void verificarProximoDiaDepoisDoDia31() {
+    public void verificarProximoDentroDoMes31DiasDepoisDoDia31() {
         Controler controler = new Controler();
         Data proximoDiaEMesRetornado = controler.proximoDia(new Data(31, 1, 2020));
         Data proximoDiaEMesEsperado = new Data(1, 2, 2020);
@@ -23,7 +23,15 @@ public class ControlerTest {
     }
 
     @Test
-    public void verificarProximoDiaDepoisDoDia30() {
+    public void verificarProximoDiaDentroDoMes30DiasDepoisDoDia29() {
+        Controler controler = new Controler();
+        Data proximoDiaRetornado = controler.proximoDia(new Data(29, 4, 2020));
+        Data proximoDiaEsperado = new Data(30, 4, 2020);
+        assertEquals(proximoDiaEsperado, proximoDiaRetornado);
+    }
+
+    @Test
+    public void verificarProximoDiaDentroDoMes30DiasDepoisDoDia30() {
         Controler controler = new Controler();
         Data proximoDiaEMesRetornado = controler.proximoDia(new Data(30, 4, 2020));
         Data proximoDiaEMesEsperado = new Data(1, 5, 2020);
@@ -33,8 +41,32 @@ public class ControlerTest {
     @Test
     public void verificarProximoDiaNaViradaDoAno() {
         Controler controler = new Controler();
-        Data proximoDiaEMesRetornado = controler.proximoDia(new Data(31, 12, 2019));
-        Data proximoDiaEMesEsperado = new Data(1, 1, 2020);
+        Data proximoDiaNaViradaDoAnoRetornado = controler.proximoDia(new Data(31, 12, 2019));
+        Data proximoDiaNaViradaDoAnoEsperado = new Data(1, 1, 2020);
+        assertEquals(proximoDiaNaViradaDoAnoEsperado, proximoDiaNaViradaDoAnoRetornado);
+    }
+
+    @Test
+    public void verificarProximoDiaDoAnoNaoBissextoDoMesDeFevereiroDepoisDoDia28() {
+        Controler controler = new Controler();
+        Data proximoDiaEMesRetornado = controler.proximoDia(new Data(28, 2, 2019));
+        Data proximoDiaEMesEsperado = new Data(1, 3, 2019);
         assertEquals(proximoDiaEMesEsperado, proximoDiaEMesRetornado);
+    }
+
+    @Test
+    public void verificarProximoDiaDoAnoBissextoDoMesDeFevereiroDepoisDoDia28() {
+        Controler controler = new Controler();
+        Data proximoDiaRetornado = controler.proximoDia(new Data(28, 2, 2020));
+        Data proximoDiaEsperado = new Data(29, 2, 2020);
+        assertEquals(proximoDiaEsperado, proximoDiaRetornado);
+    }
+
+    @Test
+    public void verificarProximoDiaDoAnoBissextoDoMesDeFevereiroDepoisDoDia29() {
+        Controler controler = new Controler();
+        Data proximoDiaRetornado = controler.proximoDia(new Data(29, 2, 2020));
+        Data proximoDiaEsperado = new Data(1, 3, 2020);
+        assertEquals(proximoDiaEsperado, proximoDiaRetornado);
     }
 }
